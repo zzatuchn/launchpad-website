fac64f92793eb37ee24e8b8a2e772a6866797ab4
Zach Zatuchni <zachzatuchni@Zachs-Air.hsd1.nj.comcast.net>
2020-12-03 00:58:55 -0500

    Deploy gh-pages

---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+
+
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg sh-navbar">
+                <a className="navbar-brand" style={{color:"rgb(256, 70, 70)"}} href="#"><h2>SafeHouse</h2></a>
+            </nav>
+        </div>
+    )
+
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,35 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/navbar'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>SafeHouse</title>
+        <meta charset="utf-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"></link>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <Navbar></Navbar>
+      <div className="home-bg-image">
+        <div className="bg-gradient">
+          <div className="main-text">
+            <h1>Fool, learn, and conquer</h1>
+            <h3>Sophisticated intrusion protection with honeypots</h3>
+            <br></br>
+            <button type="button" class="button btn-danger"><h3>Request a Consultation</h3></button>
+            <br></br><br></br>
+            <button type="button" class="button btn-dark"><h3>See How SafeHouse Works</h3></button>
+          </div>
         </div>
-      </main>
+      </div>
+      <div className="main-text">
+        <h1>Catch hackers in the act</h1>
+        <h3>We intall smart honeypots that learn as well as protect</h3>
+        <br></br><br></br>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+
     </div>
   )
 }
